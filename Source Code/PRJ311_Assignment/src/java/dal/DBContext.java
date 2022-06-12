1cb6d1726c672e7c51d5332673ef8fc9176a5377
truongxuando <themood002@gmail.com>
2022-06-12 11:05:14 +0700

    Update DBContext.java

--- Source Code/PRJ311_Assignment/src/java/dal/DBContext.java
+++ Source Code/PRJ311_Assignment/src/java/dal/DBContext.java
@@ -22,7 +22,7 @@ public abstract class DBContext<T> {
         try {
             String user = "truongdx";
             String pass = "truongdx";
-            String url = "jdbc:sqlserver://localhost\\SQLEXPRESS:1433;databaseName=rectangle";
+            String url = "jdbc:sqlserver://localhost:1433;databaseName=master";
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             connection = DriverManager.getConnection(url, user, pass);
         } catch (ClassNotFoundException ex) {
